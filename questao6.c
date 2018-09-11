328c469c1374307cf14a89269e19142cfa67648c
brun0marques <31905280+brun0marques@users.noreply.github.com>
2018-09-10 22:03:33 -0300

    Add files via upload

---
+++ questao6.c
@@ -0,0 +1,6 @@
+contador/valor/valor/endereco/endereco
+i = 0vet[0] = 1.1*(f + 0) = 1.1&vet[0] = 220B5DB0(f + 0) = 220B5DB0
+i = 1vet[1] = 2.2*(f + 1) = 2.2&vet[1] = 220B5DB4(f + 1) = 220B5DB4
+i = 2vet[2] = 3.3*(f + 2) = 3.3&vet[2] = 220B5DB8(f + 2) = 220B5DB8
+i = 3vet[3] = 4.4*(f + 3) = 4.4&vet[3] = 220B5DBC(f + 3) = 220B5DBC
+i = 4vet[4] = 5.5*(f + 4) = 5.5&vet[4] = 220B5DC0(f + 4) = 220B5DC0
