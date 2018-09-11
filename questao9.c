4da50323c075a71ad0787d2b24bffa7350cd6ad5
brun0marques <31905280+brun0marques@users.noreply.github.com>
2018-09-10 23:34:43 -0300

    Add files via upload

---
+++ questao9.c
@@ -0,0 +1,3 @@
+O primeiro programa declara e inicializa um vetor de inteiros de 3 posições. Em seguida, imprime na saída padrão os valores armazenados no vetor.
+
+O segundo programa declara e inicializa um vetor de inteiros de 3 posições. Em seguida, imprime na saída padrão os endereços de cada posição do vetor.
